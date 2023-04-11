504bd76313a600ae096a91f0c9a5b65eaeeb6f4b
rutvikkulkarni <rutvikkirankulkarni@gmail.com>
2023-04-12 00:54:50 +0530

    Initial Commit

---
+++ mydb.py
@@ -0,0 +1,14 @@
+import mysql.connector
+
+dataBase = mysql.connector.connect(
+    host='localhost',
+    user='root',
+    password='12345'
+
+)
+
+cursorObject = dataBase.cursor()
+
+cursorObject.execute(" CREATE DATABASE elder")
+
+print("all done!!")
